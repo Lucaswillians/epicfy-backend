cd71311f46942af18b63e9568b492d11198ff8de
Cristian Prochnow <cristianprochnow@gmail.com>
2024-10-22 00:46:50 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
+import cors from 'cors';
 import express, { json } from 'express';
 import { routes } from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(json());
 app.use(routes);
 
